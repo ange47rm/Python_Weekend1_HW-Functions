0057bd29833faccc91f5d2cd0a188571522c5d32
ange47rm <ange47rm@hotmail.com>
2020-11-20 19:40:09 +0000

    Passed 13 out of 23 tests.

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -43,6 +43,17 @@ def find_pet_by_name (pet_shop, name):
 
 
         
+def remove_pet_by_name(pet_shop, name):
+    for pet in pet_shop['pets']:
+        if pet['name'] == name:
+            pet_shop['pets'].remove(pet)
+
+
+def add_pet_to_stock(pet_shop, new_pet):
+    new_pet = {}
+    pet_shop['pets'].append(new_pet)
+    
+            
 
 
 
